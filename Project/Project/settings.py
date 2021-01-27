b3f5fe8250cdccb4c22793aa0c55f85e910bb1ec
Ubuntu <ubuntu@ip-172-31-15-245.ap-northeast-2.compute.internal>
2021-01-27 06:04:53 +0000

    배포앤 커밋

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -136,7 +136,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'statics'),)
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'statics'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
